a98e6e0134200d7334fbf88c09469a523a0b0964
Natalie Childs <nata.faye@gmail.com>
2023-05-04 18:33:10 -0700

    Made RestrictedAnswerPrompt

--- week5.js
+++ week5.js
@@ -1,58 +1,83 @@
 
-
-class Animal {
-    constructor(name, type) { // name = "Fluffy"; type = "cat"
-        this.name = name;
-        this.type = type;
+class RestrictedAnswerPrompt {
+    constructor(acceptableAnswers) {
+        this.acceptableAnswers = acceptableAnswers
     }
 
-    pet() {
-        alert("petting the animal!")
+    getAcceptableAnswer(input) {
+        return this.acceptableAnswers.find(answer => answer.toLowerCase() === input.toLowerCase())
+
+        // for(let i = 0; i < this.acceptableAnswers.length; i++) {
+        //     const answer = this.acceptableAnswers[i]
+        //     if(answer.toLowerCase() === input.toLowerCase()) {
+        //         return answer
+        //     }
+        // }
+
+        // for (const answer of this.acceptableAnswers) {
+        //     this.acceptableAnswers.push("fdsfds")
+        //     if (answer.toLowerCase() === input.toLowerCase()) {
+        //         return answer
+        //     }
+        // }
+
+        //return undefined
     }
 
-    feed() {
-        alert("feeding the animal!")
+    prompt(question) {
+        let answer = prompt(question)
+        let acceptedAnswer = this.getAcceptableAnswer(answer)
+        while (!acceptedAnswer) {
+            answer = prompt(question + "\nPick one of these options: " + this.acceptableAnswers.join(", "))
+            acceptedAnswer = this.getAcceptableAnswer(answer)
+        }
+        return acceptedAnswer
     }
 }
 
-// in progress object
+// function promptRestricted(question, acceptableAnswers) {
 
-// make an object with a stamp (class)
-const fluffy = new Animal("Fluffy", "cat")
-const spot = new Animal("spot", "dog")
-
-// make an object by hand
-// const fluffy = {
-//     name: "Fluffy",
-//     type: "cat",
-//     pet() {
-//         alert("petting the animal!")
-//     },
-//     feed() {
-//         alert("feeding the animal!")
+//     function getAcceptableAnswer(input) {
+//         return acceptableAnswers.find(answer => answer.toLowerCase() === input.toLowerCase())
 //     }
-// }
 
-// const spot = {
-//     name: "Spot",
-//     type: "dog",
-//     pet() {
-//         alert("petting the animal!")
-//     },
-//     feed() {
-//         alert("feeding the animal!")
+//     let answer = prompt(question)
+//     let acceptedAnswer = getAcceptableAnswer(answer)
+//     while (!acceptedAnswer) {
+//         answer = prompt(question + "\nPick one of these options: " + acceptableAnswers.join(", "))
+//         acceptedAnswer = getAcceptableAnswer(answer)
 //     }
+//     return acceptedAnswer
 // }
 
 
-// Much more common to make pure property objects by hand
-[
-    {
-        name: "spot",
-        type: "dog"
-    },
-    {
-        name: "fluffy",
-        type: "cat"
-    }
-]
+// const yesNoPrompt = new RestrictedAnswerPrompt(["Yes", "No"])
+// const shouldDelete = yesNoPrompt.prompt("Are you sure you want to delete?")
+// const toGo = yesNoPrompt.prompt("Do you want that to go?")
+// if (shouldDelete === "Yes") {
+//     alert("Deleted!")
+// }
+
+const toGo = promptRestricted("Are you sure you want to delete?", ["Yes", "No"])
+const shouldDelete = promptRestricted("Are you sure you want to delete?", ["Yes", "No"])
+
+
+const animalPrompt = new RestrictedAnswerPrompt(["DOG", "cat", "horse"])
+const favoriteAnimal = animalPrompt.prompt("What's your favorite animal?")
+
+
+const menuPrompt = new RestrictedAnswerPrompt(["1", "2", "3", "4"])
+
+
+
+
+
+
+// try { 
+//     // talk across the internet
+// } catch (error) {
+//     alert("We're having problems connecting. We'll try again in 3 seconds")
+//     // try again in 3 seconds
+// }
+
+// // okay then do other things
