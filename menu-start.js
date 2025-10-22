465f4ec74427a0760fc00f9531e049340e2d5255
Natalie Childs <nata.faye@gmail.com>
2025-10-21 19:03:17 -0700

    soups

--- menu-start.js
+++ menu-start.js
@@ -1,26 +1,18 @@
 
 class Menu {
-    constructor() {
-        this.books = []
-    }
-
     start() {
         let selection = null
-        while(selection !== "EXIT") {
-            selection = prompt("What do you want to do? EXIT or ADD or VIEW")
-            if(selection === "ADD") {
-                // Add a book
-                const bookName = prompt("What's the name of the book?")
-                const books = { name: bookName }
-                this.books.push(books)
-            } else if(selection === "VIEW") {
-                // view all the books
-                alert("Here are all the books: " + this.books.map(book => book.name))
+        while(selection !== "quit") {
+            selection = prompt("What?")
+            if(selection === "make") {
+                // make something
+            } else if(selection === "delete") {
+                // delete something
             }
         }
-        alert("Goodbye!")
     }
 }
 
 const menu = new Menu()
 menu.start()
+console.log(menu) // see what's in your properties
--- week8.js
+++ week8.js
@@ -1,78 +1,149 @@
 
-// Class is a blueprint/recipe for making objects
-// Method is a function attached to an object
-// Method is a function that's in a property
+// OOP says every function should be connected to an object (meaning a method)
+// In order to do that, we have to put every function on a class, 
+// so it will then be connected to an object
 
-class Something {
+// Alternative is: Functional Programmming - stay tuned to Week 13
 
-    // constructor is a function for putting properties on a new object
+
+// Classes are a blueprint or recipe for object(s)
+
+// The class is this -> 📃
+class FrenchOnionSoup {
     constructor() {
-        this.color = "blue"
+        console.log("Adding onions")
+        console.log("Waiting forever")
     }
 
-    sayHi() {
-        alert("hi!")
-    }
-}
-
-// "new" says to stamp out a new object using that class as the blueprint
-const myObject1 = new Something()
-
-const myObject2 = {
-    color: "blue",
-
-    // this is a method
-    sayHi: function() {
-        alert("hi!")
+    // Method - a function attached to an object
+    // (technically right here it's the recipe for that method to be on the objects)
+    // From here it CANNOT be called on its own
+    takeABite() {
+        console.log("Taking a bite of soup")
     }
 }
 
-// Same object made 2 different ways
-console.log(myObject1)
-console.log(myObject2)
+// If a function is on a class (meaning it's a method)
+// then the ONLY WAY to use it is to make an object with that class, then use .theFunctionName()
 
-// Object Oriented Programming: MAKE IT ALL METHODS!!!!
+// NO GOOD if it's a method
+//takeABite() // ERROR
 
-
-// Why objects? 
-// Convenient to group information (and possibly functions)
-
-
-// Why classes?
-// In Javascript there's not a strong reason to use them, but it can make sense
-// In modern React-y (we'll learn React at the end of the course) Javascript we don't use classes
-
-
-// Passed by Reference
-// Arrays and objects passed by reference
-// Does NOT make a copy if you set a new variable to it
-
-const myFavoriteColors = ["green", "red", "blue"] // myFavoriteColors points to this array I just made
-const daughterFavoriteColors = myFavoriteColors // daughterFavoriteColors points to the same array from line 47
-
-myFavoriteColors.push("purple")
-
-console.log(myFavoriteColors) // "green", "red", "blue", "purple"
-console.log(daughterFavoriteColors) // "green", "red", "blue", "purple"
-
-// Strings, booleans, numbers passed by value
-// Sent to new variables as a copy
-
-const myAge = 30
-const myHusbandsAge = myAge // copies the number 30 into myHusbandsAge
-
-myAge++
-
-console.log(myAge) // 31
-console.log(myHusbandsAge) // 30
+// The object is this -> 🍲
+const myBowl = new FrenchOnionSoup()
+myBowl.takeABite()
+const myFriendsBowl = new FrenchOnionSoup()
+myFriendsBowl.takeABite()
 
 
 
-// "this" in programming means wherever you are right now
-// the object you are currently inside of or connected to
 
-// the word "new" means constructor
-// the constructor returns the newly created object
+// Why isn't Javascript part of the OOP Cult (this is a joke)
+// Javascript allows you to make "free" objects
+// Object literals
+
+// We can cook from scratch
+const mySoup = {
+    ingredients: ["tomato", "onion", "broth"],
+    name: "Tomato Soup"
+}
+
+// A free function, attached to nothing
+// exists on its own
+function takeABite(soup) {
+    console.log("Taking a bite of " + soup.name)
+}
 
 
-// Generally speaking classes don't "exist"
+// OOP way
+// Pro: standardize making the object
+class Email {
+    // constructor is the steps to build the object
+    // parameters to constructor are what it needs to know to make the object
+    constructor(authorParameter, message) { // let authorParameter = "Natalie"; let message = "Hello!"
+        // Pretty much always it sets up all the properties
+        this.author = authorParameter
+        this.message = message
+        // Two buckets (variables) with the same name
+        // One is a parameter to the constructor
+        // One is a property on the under construction object (can only be accessed using something.propertName)
+
+        // this = the object I'm currently inside of
+        // What object am I inside of right now? That's what we're talking about
+        // this can (generally speaking) NEVER mean the class, only an object made with the class
+    }
+}
+
+// When you see "new" think "constructor"
+const firstEmail = new Email("Natalie", "Hello!")
+const anotherEmail = new Email("Corinne", "Hey!")
+
+// More normal Javascript way
+// Pro: Easier, and easier to read
+const secondEmail = {
+    author: "Natalie",
+    message: "Hello!"
+}
+const thirdEmail = {
+    autor: "Someone Else", // Javascript doesn't care
+    message: "Bye!"
+}
+
+// these are the same
+console.log(firstEmail)
+console.log(secondEmail)
+
+
+
+// Basic Data Types
+// boolean, string, number
+// Passed by Value = they are copied into a new variable
+
+// Complex Data Types
+// array, object
+// Passed by Reference = the variable is pointed to the object or array
+
+let myFavoriteColor = "green"
+let myDaughtersFavoriteColor = myFavoriteColor // copies "green" into the new variable
+
+myDaughtersFavoriteColor = "red"
+
+console.log(myFavoriteColor) // "green"
+console.log(myDaughtersFavoriteColor) // "red"
+
+myFavoriteColor = "purple"
+
+console.log(myFavoriteColor) // "purple"
+console.log(myDaughtersFavoriteColor) // "red"
+
+
+let myHouse = { outsideColor: "blue" }
+let myHusbandsHouse = myHouse // points the variable to the same "house" (object)
+
+myHouse.outsideColor = "pink"
+
+console.log(myHusbandsHouse) // { outsideColor: "pink" }
+
+myHusbandsHouse.outsideColor = "light purple"
+
+console.log(myHouse) // { outsideColor: "light purple" }
+
+
+// In OOP we connect our objects together with properties pointed to each other
+
+
+
+// menu = {
+//     selectedTeam: null / the Lakers team
+//     teams: [
+//         {
+//             name: "Lakers",
+//             players: [
+//                 {
+//                     name: "Mia",
+//                     position: "defense"
+//                 }
+//             ]
+//         }
+//     ]
+// }
